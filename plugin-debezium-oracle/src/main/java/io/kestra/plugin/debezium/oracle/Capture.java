aea91a34c5ab4786899da4507223fdfc842cd615
Ludovic DEHON <tchiot.ludo@gmail.com>
2022-03-28 20:15:48 +0200

    wip

---
+++ plugin-debezium-oracle/src/main/java/io/kestra/plugin/debezium/oracle/Capture.java
@@ -0,0 +1,69 @@
+package io.kestra.plugin.debezium.oracle;
+
+import io.debezium.connector.oracle.OracleConnector;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.runners.RunContext;
+import io.kestra.plugin.debezium.AbstractDebeziumTask;
+import io.micronaut.core.annotation.Introspected;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.nio.file.Path;
+import java.util.Locale;
+import java.util.Properties;
+import javax.validation.constraints.NotNull;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+public class Capture extends AbstractDebeziumTask {
+    @Schema(
+        title = "Name of the database to connect to.",
+        description = "Must be the CDB name when working with the CDB + PDB model."
+    )
+    @PluginProperty(dynamic = true)
+    @NotNull
+    protected String database;
+
+    @Schema(
+        title = "Specifies the criteria for running a snapshot when the connector starts.",
+        description = " Possible settings are:\n" +
+            "- `INITIAL`: The snapshot includes the structure and data of captured tables. Specify this value to populate topics with a complete representation of the data from the captured tables.\n" +
+            "- `SCHEMA_ONLY`: The snapshot includes only the structure of captured tables. Specify this value if you want the connector to capture data only for changes that occur after the snapshot.\n" +
+            "- `SCHEMA_ONLY_RECOVERY`: This is a recovery setting for a connector that has already been capturing changes. When you restart the connector, this setting enables recovery of a corrupted or lost database history topic. You might set it periodically to \"clean up\" a database history topic that has been growing unexpectedly. Database history topics require infinite retention. Note this mode is only safe to be used when it is guaranteed that no schema changes happened since the point in time the connector was shut down before and the point in time the snapshot is taken.\n"
+    )
+    @PluginProperty(dynamic = false)
+    @NotNull
+    @Builder.Default
+    private SnapshotMode snapshotMode = SnapshotMode.INITIAL;
+
+    @Override
+    protected boolean needDatabaseHistory() {
+        return true;
+    }
+
+    @Override
+    protected Properties properties(RunContext runContext, Path offsetFile, Path historyFile) throws Exception {
+        Properties props = super.properties(runContext, offsetFile, historyFile);
+
+        props.setProperty("connector.class", OracleConnector.class.getName());
+
+        props.put("database.dbname", runContext.render(database));
+
+        if (this.snapshotMode != null) {
+            props.setProperty("snapshot.mode", this.snapshotMode.name().toLowerCase(Locale.ROOT));
+        }
+
+        return props;
+    }
+
+    @Introspected
+    public enum SnapshotMode {
+        INITIAL,
+        SCHEMA_ONLY,
+        SCHEMA_ONLY_RECOVERY,
+    }
+}
---
+++ plugin-debezium-oracle/src/test/java/io/kestra/plugin/debezium/oracle/CaptureTest.java
@@ -0,0 +1,73 @@
+package io.kestra.plugin.debezium.oracle;
+
+import com.google.common.base.Charsets;
+import io.debezium.data.Envelope;
+import io.kestra.core.serializers.FileSerde;
+import io.kestra.core.storages.StorageInterface;
+import io.kestra.core.utils.IdUtils;
+import io.kestra.core.utils.TestsUtils;
+import io.kestra.plugin.debezium.AbstractDebeziumTask;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+
+import jakarta.inject.Inject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@MicronautTest
+class CaptureTest {
+    @Inject
+    private RunContextFactory runContextFactory;
+
+    @Inject
+    private StorageInterface storageInterface;
+
+    @SuppressWarnings("unchecked")
+    @Test
+    void run() throws Exception {
+        Capture task = Capture.builder()
+            .id(IdUtils.create())
+            .type(Capture.class.getName())
+            .hostname("127.0.0.1")
+            .port("41521")
+            .username("dbzuser")
+            .password("dbz")
+            .database("XE")
+            .maxRecords(19)
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, task, Map.of());
+        AbstractDebeziumTask.Output runOutput = task.run(runContext);
+
+        assertThat(runOutput.getSize(), is(19));
+
+        List<Map<String, Object>> employee = new ArrayList<>();
+        FileSerde.reader(new BufferedReader(new InputStreamReader(storageInterface.get(runOutput.getUris().get("second.EmployeeTerritory")))), r -> employee.add((Map<String, Object>) r));
+
+        List<Map<String, Object>> types = new ArrayList<>();
+        FileSerde.reader(new BufferedReader(new InputStreamReader(storageInterface.get(runOutput.getUris().get("kestra.mysql_types")))), r -> types.add((Map<String, Object>) r));
+
+        IOUtils.toString(storageInterface.get(runOutput.getUris().get("kestra.mysql_types")), Charsets.UTF_8);
+        assertThat(employee.size(), is(14));
+        assertThat(employee.stream().filter(o -> !((Boolean) o.get("deleted"))).count(), is(7L));
+
+        FileSerde.reader(new BufferedReader(new InputStreamReader(storageInterface.get(runOutput.getUris().get("second.EmployeeTerritory")))), r -> employee.add((Map<String, Object>) r));
+        assertThat(types.size(), is(5));
+        assertThat(types.stream().filter(o -> !((Boolean) o.get("deleted"))).count(), is(4L));
+
+        // rerun state will prevent new records
+        runOutput = task.run(runContext);
+        assertThat(runOutput.getSize(), is(0));
+    }
+}
