5374e6028dd911ca3c6c4b0009d4d795105f4167
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:29:27 +0100

    feat: plugin categories and icons

---
+++ plugin-debezium-mysql/src/main/java/io/kestra/plugin/debezium/mysql/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Debezium with MySQL.\n" +
+        "Debezium is an open source distributed platform for change data capture.",
+    categories = PluginSubGroup.PluginCategory.DATABASE
+)
+package io.kestra.plugin.debezium.mysql;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
