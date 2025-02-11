295e51cb24e601d3954d0e36d9c4e49da3f18c9c
Mathieu Gabelle <mgabelle@kestra.io>
2025-02-11 19:21:33 +0100

    fix: ad runnerUtils to run flaky test

--- plugin-debezium-mysql/src/test/java/io/kestra/plugin/debezium/mysql/CaptureTest.java
+++ plugin-debezium-mysql/src/test/java/io/kestra/plugin/debezium/mysql/CaptureTest.java
@@ -1,7 +1,13 @@
 package io.kestra.plugin.debezium.mysql;
 
 import com.google.common.base.Charsets;
+import io.kestra.core.models.executions.Execution;
+import io.kestra.core.models.flows.State;
 import io.kestra.core.models.property.Property;
+import io.kestra.core.queues.QueueException;
+import io.kestra.core.repositories.LocalFlowRepositoryLoader;
+import io.kestra.core.runners.RunnerUtils;
+import io.kestra.core.runners.StandAloneRunner;
 import io.kestra.core.serializers.FileSerde;
 import io.kestra.core.storages.StorageInterface;
 import io.kestra.core.utils.IdUtils;
@@ -10,6 +16,7 @@ import io.kestra.plugin.debezium.AbstractDebeziumTask;
 import io.kestra.plugin.debezium.AbstractDebeziumTest;
 import io.kestra.core.junit.annotations.KestraTest;
 import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
@@ -17,10 +24,19 @@ import io.kestra.core.runners.RunContextFactory;
 import jakarta.inject.Inject;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.TimeoutException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -33,6 +49,73 @@ class CaptureTest extends AbstractDebeziumTest {
 
     @Inject
     private StorageInterface storageInterface;
+
+    @Inject
+    protected StandAloneRunner runner;
+
+    @Inject
+    protected RunnerUtils runnerUtils;
+
+    @Inject
+    protected LocalFlowRepositoryLoader repositoryLoader;
+
+    @BeforeEach
+    protected void init() throws IOException, URISyntaxException {
+        repositoryLoader.load(Objects.requireNonNull(CaptureTest.class.getClassLoader().getResource("flows")));
+        this.runner.run();
+    }
+
+    @Test
+    void flow() throws TimeoutException, QueueException, SQLException, IOException, URISyntaxException {
+        // init database
+        executeSqlScript("scripts/mysql.sql");
+
+        Execution execution = runnerUtils.runOne(
+            null,
+            "io.kestra.tests",
+            "capture-mysql",
+            null,
+            (f, e) -> Map.of(
+                "username", getUsername(),
+                "password", getPassword()
+            ),
+            Duration.ofMinutes(10)
+        );
+
+        assertThat(execution.getState().getCurrent(), is(State.Type.SUCCESS));
+
+
+        Map<String, Object> output = execution.getTaskRunList().getFirst().getOutputs();
+
+        assertThat(output.get("size"), is(5));
+
+        List<Map<String, Object>> events = new ArrayList<>();
+        Map<String, URI> uris = (Map<String, URI>) output.get("uris");
+
+        FileSerde.reader(new BufferedReader(new InputStreamReader(storageInterface.get(null, null, uris.get("kestra.events")))), r -> events.add((Map<String, Object>) r));
+
+        IOUtils.toString(storageInterface.get(null, null, uris.get("kestra.events")), StandardCharsets.UTF_8);
+        assertThat(events.size(), is(5));
+        assertTrue(events.stream().anyMatch(map -> map.get("event_title").equals("Machine Head")));
+        assertTrue(events.stream().anyMatch(map -> map.get("event_title").equals("Dropkick Murphys")));
+        assertTrue(events.stream().anyMatch(map -> map.get("event_title").equals("Pink Floyd")));
+        assertTrue(events.stream().anyMatch(map -> map.get("event_title").equals("TV show")));
+        assertTrue(events.stream().anyMatch(map -> map.get("event_title").equals("Nothing")));
+
+        // rerun state will prevent new records
+        runnerUtils.runOne(
+            null,
+            "io.kestra.tests",
+            "capture-mysql",
+            null,
+            (f, e) -> Map.of(
+                "username", getUsername(),
+                "password", getPassword()
+            ),
+            Duration.ofMinutes(10)
+        );
+        assertThat(output.get("size"), is(5));
+    }
 
     @Override
     protected String getUrl() {
@@ -47,45 +130,5 @@ class CaptureTest extends AbstractDebeziumTest {
     @Override
     protected String getPassword() {
         return "mysql_passwd";
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    void run() throws Exception {
-        // init database
-        executeSqlScript("scripts/mysql.sql");
-
-        Capture task = Capture.builder()
-            .id(IdUtils.create())
-            .type(Capture.class.getName())
-            .serverId(Property.of("123456789"))
-            .snapshotMode(Property.of(MysqlInterface.SnapshotMode.NEVER))
-            .hostname(Property.of("127.0.0.1"))
-            .port(Property.of("63306"))
-            .username(Property.of(getUsername()))
-            .password(Property.of(getPassword()))
-            .maxRecords(Property.of(5))
-            .includedTables(List.of("kestra.events"))
-            .build();
-
-        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, task, Map.of());
-        AbstractDebeziumTask.Output runOutput = task.run(runContext);
-
-        assertThat(runOutput.getSize(), is(5));
-
-        List<Map<String, Object>> events = new ArrayList<>();
-        FileSerde.reader(new BufferedReader(new InputStreamReader(storageInterface.get(null, null, runOutput.getUris().get("kestra.events")))), r -> events.add((Map<String, Object>) r));
-
-        IOUtils.toString(storageInterface.get(null, null, runOutput.getUris().get("kestra.events")), Charsets.UTF_8);
-        assertThat(events.size(), is(5));
-        assertTrue(events.stream().anyMatch(map -> map.get("event_title").equals("Machine Head")));
-        assertTrue(events.stream().anyMatch(map -> map.get("event_title").equals("Dropkick Murphys")));
-        assertTrue(events.stream().anyMatch(map -> map.get("event_title").equals("Pink Floyd")));
-        assertTrue(events.stream().anyMatch(map -> map.get("event_title").equals("TV show")));
-        assertTrue(events.stream().anyMatch(map -> map.get("event_title").equals("Nothing")));
-
-        // rerun state will prevent new records
-        runOutput = task.run(runContext);
-        assertThat(runOutput.getSize(), is(0));
     }
 }
