f3f02dc8308c868b3ae7c2c515f487682c56f8d8
ben8t <pimpaudben@gmail.com>
2025-12-30 15:35:43 +0100

    chore: update plugin categories

--- plugin-debezium-oracle/src/main/java/io/kestra/plugin/debezium/oracle/package-info.java
+++ plugin-debezium-oracle/src/main/java/io/kestra/plugin/debezium/oracle/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Debezium with Oracle.\n" +
         "Debezium is an open source distributed platform for change data capture.",
-    categories = PluginSubGroup.PluginCategory.DATABASE
+    categories = PluginSubGroup.PluginCategory.DATABASE,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA
+    }
 )
 package io.kestra.plugin.debezium.oracle;
 
