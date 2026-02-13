7b6322568d0a44fa8029d87f01db1a86c2ef7747
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 12:04:03 +0100

    chore: update plugin categories (#140)
    * chore: update plugin categories
    
    * chore: fix plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>

--- plugin-debezium-mongodb/src/main/java/io/kestra/plugin/debezium/mongodb/package-info.java
+++ plugin-debezium-mongodb/src/main/java/io/kestra/plugin/debezium/mongodb/package-info.java
@@ -1,7 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Debezium with MongoDB.\n" +
         "Debezium is an open source distributed platform for change data capture.",
-    categories = PluginSubGroup.PluginCategory.DATABASE
+    categories = {
+        PluginSubGroup.PluginCategory.DATA
+    }
 )
 package io.kestra.plugin.debezium.mongodb;
 
